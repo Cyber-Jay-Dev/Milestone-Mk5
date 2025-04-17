74a58dc63226ca79d5b07ed5e874d0f43c5c56f9
JAMES CARL GONZALES <gonzalesjamescarl5@gmail.com>
2025-04-17 13:51:08 +0800

    mk-7

--- app/src/main/java/com/example/milestonemk_4/activitiesUI/project_detail.java
+++ app/src/main/java/com/example/milestonemk_4/activitiesUI/project_detail.java
@@ -13,7 +13,6 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
